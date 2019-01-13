aa84b9e826387ee80b10cda49ed4bc92c3232b18
hallel1 <halleli100@gmail.com>
2019-01-13 11:04:57 +0200

    main and data

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 import csv_handle as csv_org
+
+path = 'hearts.csv'
+df_org = pd.read_csv(path)
+dfCopy=df_org.__deepcopy__()
+
+df = dfCopy.replace(np.nan, '', regex=True)# replace nan values with ''
+print(df)
