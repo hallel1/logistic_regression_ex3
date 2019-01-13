4b20e11a02a61cd8253e10f755c3da73a56ea68f
hallel1 <halleli100@gmail.com>
2019-01-13 18:42:45 +0200

    kf finisf in main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from numpy.distutils.system_info import xft_info
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 import csv_handle as csv_org
 import numpy as np
 import pandas as pd
@@ -16,9 +17,13 @@ if __name__ == "__main__":
     csv_org.insert_col_df(df)
     csv_org.normalizationAll(df)
 
+    print(df)
+
     XMatrix = csv_org.x_matrix(df)
     y = csv_org.y_vector(df)
+    '''
     X_train, X_test, y_train, y_test = train_test_split(XMatrix, y, test_size=0.30, random_state=42)
+     
     v_theta = np.zeros(df.shape[1])  # logreg.random_theta(df)
 
 
@@ -29,3 +34,33 @@ if __name__ == "__main__":
 
     print(X_test)
     print(logreg.predict_proba(X_test))#check the result of the model about x-test
+    '''
+    kf = KFold(n_splits=10)  # Define the split - into 10 folds
+    kf.get_n_splits(XMatrix)  # returns the number of splitting iterations in the cross-validator
+    print(kf)
+
+
+    for train_index, test_index in kf.split(XMatrix):
+         #print('TRAIN:', train_index, 'TEST:', test_index)
+         X_train = []
+         X_test = []
+         y_train = []
+         y_test = []
+         for i in range(train_index.__len__()):
+              indexTrain = train_index[i]
+              X_train.append(XMatrix[indexTrain])
+              y_train.append(XMatrix[indexTrain])
+
+         for j in range(test_index.__len__()):
+              indexTest=test_index[j]
+              X_test.append(XMatrix[indexTest])
+              y_test.append(XMatrix[indexTest])
+
+
+
+
+         #X_train = XMatrix[train_index]
+        # X_test = XMatrix[test_index]
+        # y_train = y[train_index]
+        # y_test = y[test_index]
+
