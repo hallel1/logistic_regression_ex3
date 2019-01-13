6cbb28f1aaa28b9df8d86d5b51bad8bd9b6df07f
hallel1 <halleli100@gmail.com>
2019-01-13 18:51:31 +0200

    kf finish

--- lgReg_handle.py
+++ lgReg_handle.py
@@ -1,3 +1,24 @@
+from sklearn.model_selection import KFold
+
+def kFold(XMatrix,y):
+    kf = KFold(n_splits=10)  # Define the split - into 10 folds
+    kf.get_n_splits(XMatrix)  # returns the number of splitting iterations in the cross-validator
+    print(kf)
+
+    for train_index, test_index in kf.split(XMatrix):
+        # print('TRAIN:', train_index, 'TEST:', test_index)
+        X_train = []
+        X_test = []
+        y_train = []
+        y_test = []
+        for i in range(train_index.__len__()):
+            indexTrain = train_index[i]
+            X_train.append(XMatrix[indexTrain])
+            y_train.append(y[indexTrain])
+
+        for j in range(test_index.__len__()):
+            indexTest = test_index[j]
+            X_test.append(XMatrix[indexTest])
+            y_test.append(y[indexTest])
 
 
-#df kFold():
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 import csv_handle as csv_org
+import lgReg_handle as lgr
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
@@ -35,32 +36,5 @@ if __name__ == "__main__":
     print(X_test)
     print(logreg.predict_proba(X_test))#check the result of the model about x-test
     '''
-    kf = KFold(n_splits=10)  # Define the split - into 10 folds
-    kf.get_n_splits(XMatrix)  # returns the number of splitting iterations in the cross-validator
-    print(kf)
 
-
-    for train_index, test_index in kf.split(XMatrix):
-         #print('TRAIN:', train_index, 'TEST:', test_index)
-         X_train = []
-         X_test = []
-         y_train = []
-         y_test = []
-         for i in range(train_index.__len__()):
-              indexTrain = train_index[i]
-              X_train.append(XMatrix[indexTrain])
-              y_train.append(XMatrix[indexTrain])
-
-         for j in range(test_index.__len__()):
-              indexTest=test_index[j]
-              X_test.append(XMatrix[indexTest])
-              y_test.append(XMatrix[indexTest])
-
-
-
-
-         #X_train = XMatrix[train_index]
-        # X_test = XMatrix[test_index]
-        # y_train = y[train_index]
-        # y_test = y[test_index]
-
+    lgr.kFold(XMatrix,y)
